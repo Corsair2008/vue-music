dc0ee65f97440a3077e88da7316e1a57bb418cd1
Corsair <mylikes2008@126.com>
2019-10-09 08:39:09 +0800

    Initialize Singer Page

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,48 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+export default {
+  name: 'Scroll',
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    data: {
+      type: Array,
+      default: null
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this._initScroll()
+    }, 20)
+  },
+  methods: {
+    _initScroll () {
+      if (!this.$refs.wrapper) {
+        return
+      }
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        scrollX: false,
+        scrollY: true,
+        probeType: this.probeType,
+        click: this.click
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,26 +1,82 @@
 <template>
-  <div>
-    歌手
+  <div class="singer">
+    <listview :data="singers"></listview>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import { getSingerList } from '@/api/singer.js'
+import { getSingerList } from '@/api/singer'
+import { ERR_OK } from '@/api/config'
+import Singer from 'common/js/singer'
+import Listview from '@/base/listview/listview'
+
+const HOT_SINGER = '热门'
+const HOT_SINGER_LEN = 10
+
 export default {
   name: 'Singer',
-  mounted () {
+  data () {
+    return {
+      singers: []
+    }
+  },
+  components: {
+    Listview
+  },
+  created () {
     this._getSingerList()
   },
   methods: {
     _getSingerList () {
       getSingerList().then((res) => {
-        console.log(res)
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+        }
       })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_SINGER,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+        }
+        let key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+      })
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_SINGER) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
     }
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
