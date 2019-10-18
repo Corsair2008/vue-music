fc727f8535f21e7828e2747c464946c26b917a72
Corsair <mylikes2008@126.com>
2019-10-18 08:38:59 +0800

    optimize singer detail page

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -5,9 +5,14 @@
   </div>
   <h1 class="title" v-html="title"></h1>
   <div :style="bgStyle" ref="bgImage" class="bg-image" >
-    <div class="filter"></div>
+    <div ref="filter" class="filter"></div>
   </div>
-  <scroll :data="songList" ref="list" class="list">
+  <scroll :data="songList"
+          :listen-scroll="listenScroll"
+          @scroll="scroll"
+          ref="list"
+          class="list"
+  >
     <div class="song-list-wrapper">
       <song-list :song-list="songList"></song-list>
     </div>
@@ -18,8 +23,16 @@
 <script type="text/ecmascript-6">
 import SongList from '@/components/song-list/song-list'
 import Scroll from '@/base/scroll/scroll'
+
+const TOP_HEIGHT = 40
+
 export default {
   name: 'MusicList',
+  data () {
+    return {
+      scrollY
+    }
+  },
   props: {
     songList: {
       type: Array,
@@ -37,14 +50,44 @@ export default {
   methods: {
     back () {
       this.$router.back()
+    },
+    scroll (pos) {
+      this.scrollY = pos.y
     }
   },
+  created () {
+    this.listenScroll = true
+  },
   mounted () {
-    this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+    this.bgImageHeight = this.$refs.bgImage.clientHeight
+    this.bgImageMaxScrollHeight = TOP_HEIGHT - this.bgImageHeight
+    this.$refs.list.$el.style.top = `${this.bgImageHeight}px`
   },
   computed: {
     bgStyle () {
-      return `background-image:url(${this.bgImage})`
+      return `background-image:url(${this.bgImage}`
+    }
+  },
+  watch: {
+    scrollY (newY) {
+      let zIndex = 0
+      let percent = newY / this.bgImageHeight
+      if (newY > 0) {
+        zIndex = 10
+        let scale = 1 + percent
+        this.$refs.bgImage.style.transform = `scale(${scale})`
+        this.$refs.bgImage.style.webkitTransform = `scale(${scale})`
+      } else {
+        let blur = Math.min(20 * percent, 20)
+        let height = Math.max(this.bgImageMaxScrollHeight, newY)
+        let opacity = Math.max((1 - height / this.bgImageMaxScrollHeight), 0.2)
+        this.$refs.bgImage.style.transform = `translate3d(0, ${height}px, 0)`
+        this.$refs.bgImage.style.webkitTransform = `translate3d(0, ${height}px, 0)`
+        this.$refs.filter.style.backdropFilter = `blur(${blur}px)`
+        this.$refs.filter.style.webkitBackdropFilter = `blur(${blur}px)`
+        this.$refs.bgImage.style.opacity = opacity
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
     }
   },
   components: {
